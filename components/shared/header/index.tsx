c79c29562e6dc22607ef52ecab0ab8d9df337ab6
tt3887tt <tt3887tt@gmail.com>
2025-04-27 17:17:37 +0700

    update

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
+
 
 
 const Header = () => {
@@ -19,17 +19,7 @@ const Header = () => {
                 </Link>
             </div>
             <div className="space-x-2">
-                <ModeToggle />
-                <Button asChild variant="ghost" >
-                    <Link href="/cart">
-                        <ShoppingCart /> Cart
-                    </Link>
-                </Button>
-                <Button asChild variant="ghost" >
-                    <Link href="/sign-in">
-                        <UserIcon /> Sign In
-                    </Link>
-                </Button>
+                <Menu />
             </div>
         </div>
 
